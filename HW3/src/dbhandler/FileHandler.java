e2c6936caf7e409229df47f00f9de953bf90252d
Anna Bondarenko <postportal@mail.ru>
2019-04-01 19:22:10 +0300

    changed according to reviewer's comment

--- HW3/src/dbhandler/FileHandler.java
+++ HW3/src/dbhandler/FileHandler.java
@@ -76,18 +76,25 @@ public class FileHandler {
 	    }
 	}
 	
-	public static String readUsingBufferedReader(String fileName) throws IOException{
-        BufferedReader reader = new BufferedReader( new FileReader (fileName));
-        String line = null;
-        StringBuilder stringBuilder = new StringBuilder();
-        String ls = System.getProperty("line.separator");
-        while( ( line = reader.readLine() ) != null ) {
-            stringBuilder.append( line );
-            stringBuilder.append( ls );
-        }
+	public static String readUsingBufferedReader(String fileName) {
         
-        stringBuilder.deleteCharAt(stringBuilder.length()-1);
-        reader.close();
-        return stringBuilder.toString();
+        try {
+        	BufferedReader reader = new BufferedReader( new FileReader (fileName));
+            String line = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            String ls = System.getProperty("line.separator");
+        	while( ( line = reader.readLine() ) != null ) {
+			    stringBuilder.append( line );
+			    stringBuilder.append( ls );
+			}
+			stringBuilder.deleteCharAt(stringBuilder.length()-1);
+	        reader.close();
+	        return stringBuilder.toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;  
+        
     }
 }
