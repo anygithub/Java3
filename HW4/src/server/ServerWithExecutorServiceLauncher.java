86e8d38f1b98450475dded48726b2d4cbfb0e2a5
Anna Bondarenko <postportal@mail.ru>
2019-04-03 14:58:34 +0300

    class ServerWithExecutorServiceLauncher

---
+++ HW4/src/server/ServerWithExecutorServiceLauncher.java
@@ -0,0 +1,8 @@
+package server;
+
+public class ServerWithExecutorServiceLauncher {
+	  public static void main(String[] args)
+	  {
+		  ServerWithExecutorService serverLauncher = new ServerWithExecutorService();
+	  }
+}
