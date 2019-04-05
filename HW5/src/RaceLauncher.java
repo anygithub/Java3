4429b124db7ac84f84a7038caabab6d3d36b3fc9
Anna Bondarenko <postportal@mail.ru>
2019-04-05 17:15:23 +0300

    changes for homework

--- HW5/src/RaceLauncher.java
+++ HW5/src/RaceLauncher.java
@@ -1,7 +1,13 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 
 public class RaceLauncher {
 	public static final int CARS_COUNT = 4;
-	public static void main(String[] args) {
+	public static final CountDownLatch START = new CountDownLatch(CARS_COUNT + 1);
+    public static final Semaphore TUNNEL = new Semaphore(CARS_COUNT / 2, false);
+    public static final CountDownLatch FINISH = new CountDownLatch(CARS_COUNT);
+	
+    public static void main(String[] args) throws InterruptedException {
 		System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
@@ -11,7 +17,14 @@ public class RaceLauncher {
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        
+        while (START.getCount() > 1)   
+            Thread.sleep(100);         
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        START.countDown();             
+
+        while (FINISH.getCount() > 0);       
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
 	}
 }
