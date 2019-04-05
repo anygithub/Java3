3d8b40af180579abf174d3b595771a595355a1f1
Anna Bondarenko <postportal@mail.ru>
2019-04-05 17:14:40 +0300

    changes for hw

--- HW5/src/Car.java
+++ HW5/src/Car.java
@@ -6,29 +6,46 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
-    public String getName() {
-        return name;
-    }
-    public int getSpeed() {
-        return speed;
-    }
+    
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getSpeed() {
+        return speed;
+    }
+    
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            
+            RaceLauncher.START.countDown();    
+            RaceLauncher.START.await();        
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        
+        if (RaceLauncher.FINISH.getCount() == RaceLauncher.CARS_COUNT)   
+            System.out.println(name + " - WIN");
+
+        try {
+            RaceLauncher.FINISH.countDown();   
+            RaceLauncher.FINISH.await();       
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
